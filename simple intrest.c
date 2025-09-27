0470d402a4eb0888c281efc99f59baf04065c181
varshi <keerthiatukuri@gmail.com>
2025-09-27 14:06:35 +0530

    initial3

---
+++ simple intrest.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+    int p,t;
+    float result,r;
+    printf("Enter value of p:");
+printf("Enter value of t:");
+scanf("%d",&t);
+printf("Enter value of r:");
+scanf("%f",&r);
+result=(p*t*r)/100;
+printf("Result is:%f",result);
+}
