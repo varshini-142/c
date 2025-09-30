b6088d706d2a15094a1cd28cc01dec73c0c09dae
Mustafa1765 <shaikmustafaali1765@gmail.com>
2025-09-30 21:29:06 +0530

    Create modifiedarray.c

---
+++ modifiedarray.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int func(int *p, int n) {
+    for (int i = 0; i < n; i++) {
+        printf("%d ", *(p + i));
+    }
+    return 0;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n]; // ✅ declare the array with size n
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    func(arr, n);
+
+    return 0;
+}
