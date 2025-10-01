2947f15514a67704a0ad186fe03daa3478898704
varshini-142 <keerthiatukuri@gmail.com>
2025-10-01 21:36:33 +0530

    reverse7.c

---
+++ reverse.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int num, reversedNumber = 0, remainder;
+
+    printf("Enter an integer: ");
+    scanf("%d", &num);
+
+    while (num != 0) {
+        remainder = num % 10;
+    }
+    printf("Reversed Number = %d\n", reversedNumber);
+
+    return 0;
+}
