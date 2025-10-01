708700187d5529ccfbff695066cdddaa54e6f8a6
Amit Gorai <amitgorai14364@gmail.com>
2025-10-01 22:24:25 +0530

    resolved the logic of fibonacci series (# 16)

--- fib1.c
+++ fib1.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
     int n, first = 0, second = 1, nextTerm;
-
     printf("Enter the number of terms: ");
     scanf("%d", &n);
-
-    printf("Fibonacci Series: ");
-    for (int i = 1; i <= n; ++i) {
+    printf("Fibonacci Series: "); // initailly printing first and second i.e(0,1)
+    printf("%d %d ", first,second);
+    // Start loop from 3 because first two Fibonacci numbers are already printed
+    for (int i = 3; i <= n; i++)
+    {
+        // Calculate next term by adding previous two terms
         nextTerm = first + second;
+        printf("%d ", nextTerm);
         first = second;
         second = nextTerm;
     }
