7b616d69c0bc51b4993184a36484a96764420da8
varshini-142 <keerthiatukuri@gmail.com>
2025-10-01 21:49:45 +0530

    Create fib1.c

---
+++ fib1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, first = 0, second = 1, nextTerm;
+
+    printf("Enter the number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: ");
+    for (int i = 1; i <= n; ++i) {
+        nextTerm = first + second;
+        first = second;
+        second = nextTerm;
+    }
+    printf("\n");
+    return 0;
+}
